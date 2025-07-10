8bed4b9d905912775d3983e8067378ee7cf97018
Masoud Abbasi <masoud.abbasi.71@gmail.com>
2025-07-10 16:29:28 +0330

    All

---
+++ front-end/src/app/not-found.js
@@ -0,0 +1,25 @@
+"use client";
+
+import Image from "next/image";
+import styles from "@app/not-found.module.css";
+
+function notFound() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image
+          src="/images/404.webp"
+          width={1000}
+          height={1000}
+          alt="404 - not found"
+        />
+      </div>
+      <div className={styles.buttonContainer}>
+        <p>صفحه مورد نظر یافت نشد!</p>
+        <button>بازگشت به صفحه اصلی</button>
+      </div>
+    </div>
+  );
+}
+
+export default notFound;
